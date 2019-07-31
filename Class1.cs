792a1e55e8a2b4ce789fc75d27e33f0a36367639
kevinAllendes <kevinallendes1993@gmail.com>
2019-07-31 12:45:48 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Data.SQLClient;
+
+
+
+namespace Utilidades
+{
+    public class Class1
+    {
+    }
+
+    /*
+    public static DataSet ConectarBDD(string ruta)
+    {
+        try
+        {
+            SqlConnection coneccion = new SqlConnection("Data Source =.; Initial Catalog = DB_sipalki_data; Integrated Security = True");
+            coneccion.Open();
+            MessageBox.Show("Se ha conectado correctamente", "Doyo AMK");
+        }
+        catch
+        {
+            MessageBox.Show("No se ha podido conectar con la BDD", "Doyo AMK");
+
+        }
+
+    
+    }
+*/
+}
